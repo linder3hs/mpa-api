a47ae32427e9dcb5ec99ec36ac7eab0be66e4874
linder3hs <linderhassinger00@gmail.com>
2022-11-23 16:29:00 -0500

    feat: test pg

--- server.mjs
+++ server.mjs
@@ -1,16 +1,52 @@
 import express from "express";
+import { Client } from "pg";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "Funciona",
+async function connection() {
+  const client = Client({
+    host: "dpg-cdv8jspa6gdsa666s1l0-a",
+    port: 5432,
+    user: "root",
+    password: "M0vVsbMs3ZHUda5zoyEew3YcdEFzyEkV",
+    database: "codigo",
   });
+
+  return await client.connect();
+}
+
+app.get("/", async (req, res) => {
+  const text = "INSERT INTO users(name, email) VALUES($1, $2) RETURNING *";
+  const values = ["brianc", "brian.m.carlson@gmail.com"];
+
+  const client = await connection();
+
+  try {
+    const res = await client.query(text, values);
+    console.log(res.rows[0]);
+    res.json({
+      message: res.rows[0],
+    });
+    // { name: 'brianc', email: 'brian.m.carlson@gmail.com' }
+  } catch (err) {
+    console.log(err.stack);
+  }
 });
 
-app.get("/users", (req, res) => {
+app.get("/users", async (req, res) => {
+  const query = {
+    name: "users",
+    text: "SELECT * FROM user WHERE",
+  };
+
+  const client = await connection();
+
+  // promise
+  const res = await client.query(query);
+  const data = res.rows[0];
+
   res.json({
-    message: "Funciona con usuarios",
+    message: data,
   });
 });
 
