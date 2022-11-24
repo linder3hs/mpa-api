f4d10710a3b516b049c63e4c70f3d82a9263eee9
linder3hs <linderhassinger00@gmail.com>
2022-11-23 22:13:15 -0500

    feat: add corse

--- server.mjs
+++ server.mjs
@@ -1,10 +1,12 @@
 import express from "express";
 import userRouter from "./routes/users.mjs";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.user(cors());
 
 app.use("/api/v1/user", userRouter);
 
